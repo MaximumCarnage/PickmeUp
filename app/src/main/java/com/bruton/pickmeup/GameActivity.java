85f618cb5cf29beca40388468c2915673b83d95f
MaximumCarnage <dylansbruton@gmail.com>
2018-05-02 20:00:19 -0400

    gameview added

--- app/src/main/java/com/bruton/pickmeup/GameActivity.java
+++ app/src/main/java/com/bruton/pickmeup/GameActivity.java
@@ -1,13 +1,27 @@
 package com.bruton.pickmeup;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
-
+public class GameActivity extends Activity {
+    private GameView m_gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        m_gameView = new GameView(this);
+        setContentView(m_gameView);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        m_gameView.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        m_gameView.resume();
     }
 }
--- app/src/main/java/com/bruton/pickmeup/GameView.java
+++ app/src/main/java/com/bruton/pickmeup/GameView.java
@@ -1,4 +1,50 @@
 package com.bruton.pickmeup;
 
-public class GameView {
+import android.content.Context;
+import android.util.Log;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+    private volatile boolean m_playing;
+    private Thread m_gameThread = null;
+
+    public GameView(Context context){
+        super(context);
+    }
+
+    @Override
+    public void run(){
+        while(m_playing){
+            update();
+            draw();
+            control();
+        }
+
+    }
+
+    private void update(){
+
+    }
+
+    private void draw(){
+
+    }
+    private void control(){
+
+    }
+
+
+    public void pause(){
+        m_playing = false;
+        try {
+            m_gameThread.join();
+        } catch(InterruptedException e){
+           Log.d("error in GameView.Java - Pause", e.getMessage());
+        }
+    }
+    public void resume(){
+        m_playing = true;
+        m_gameThread = new Thread(this);
+        m_gameThread.start();
+    }
 }
